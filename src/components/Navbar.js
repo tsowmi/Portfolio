20f15c4daa0d886843883c74574dcaf54c8d8ac4
Sowmya <tuttaguntasowmya@gmail.com>
2023-07-22 16:52:10 +0530

    commit 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/tsowmi/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
